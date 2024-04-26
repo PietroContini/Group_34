d7b0517459d99760e12f8b0cb0dedeb1f296b520
UniLucaPici <126816792+UniLucaPici@users.noreply.github.com>
2024-04-26 19:17:41 +0200

    Piccola correzione a CartaRisorsa, necessaria

--- CodexGruppo34/src/componentiGioco/CartaRisorsa.java
+++ CodexGruppo34/src/componentiGioco/CartaRisorsa.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 public class CartaRisorsa extends Carta {
 	
-    public CartaRisorsa(Angolo angolo1,Angolo angolo2,Angolo angolo3,Angolo angolo4) {
+	private int puntiRis;
+	
+    public CartaRisorsa(Angolo angolo1,Angolo angolo2,Angolo angolo3,Angolo angolo4,int punti) {
    	 
     	super(angolo1,angolo2,angolo3,angolo4);
- 
+    	this.puntiRis=punti;
 
     }
 
