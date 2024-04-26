4963a927e1b858895cf1c320ebe5fd04837bb96a
PietroContini <p.contini@studenti.unibg.it>
2024-04-26 17:48:51 +0200

    costruttore carte oro
    abbiamo creato la prima carta oro
    
    Co-Authored-By: FrancescoScarpellini <167545315+FrancescoScarpellini@users.noreply.github.com>
    Co-Authored-By: Picinali Luca <126816792+UniLucaPici@users.noreply.github.com>

--- CodexGruppo34/src/main/Main.java
+++ CodexGruppo34/src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.util.ArrayList;
+
 import componentiGioco.Angolo;
 import componentiGioco.Carta;
+import componentiGioco.CartaOro;
 import componentiGioco.CartaRisorsa;
 import componentiGioco.Posizione;
 import componentiGioco.Risorsa;
@@ -18,7 +21,15 @@ public class Main {
 		Angolo angolo4 = new Angolo(Posizione.BassoDestra,null);
 		CartaRisorsa carta1risorsa = new CartaRisorsa(angolo1,angolo2,angolo3,angolo4);
 		
-		
+		angolo1 = new Angolo(Posizione.AltoSinistra,null);
+		angolo2 = new Angolo(Posizione.BassoSinistra,Risorsa.vuoto);
+		angolo3 = new Angolo(Posizione.AltoDestra,Risorsa.vuoto);
+		angolo4 = new Angolo(Posizione.BassoDestra,Risorsa.piuma);
+		ArrayList<Risorsa> lista = new ArrayList<> ();
+		lista.add(Risorsa.fungo);
+		lista.add(Risorsa.fungo);
+		lista.add(Risorsa.lupo);
+		CartaOro carta1oro = new CartaOro (angolo1,angolo2,angolo3,angolo4,lista,1,Risorsa.piuma);
 	}
 
 }
