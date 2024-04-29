233b0faf78164a675bdbe14c51ab07eb7ec575a3
UniLucaPici <126816792+UniLucaPici@users.noreply.github.com>
2024-04-29 15:44:14 +0200

    Correzzione mazzo

--- CodexGruppo34/src/gestioneGioco/Mazzo.java
+++ CodexGruppo34/src/gestioneGioco/Mazzo.java
@@ -6,8 +6,11 @@ public class Mazzo {
 	
     private Carta [] carte;
     
-    public Mazzo(int numero) {
+    public Mazzo(int numero,Carta[] carte ) {
     	this.carte=new Carta[numero];
+    	this.carte=carte;
+    	
+    	
     }
 
 }
