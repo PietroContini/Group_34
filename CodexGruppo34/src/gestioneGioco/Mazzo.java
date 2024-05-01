f815d6488cb9bce4e52b6f33cd4d08aa6a2ef9d0
FrancescoScarpellini <167545315+FrancescoScarpellini@users.noreply.github.com>
2024-05-01 16:00:04 +0200

    metodo mischiaaMazzo
    creazione metodo mischiaMazzo classe mazzo

--- CodexGruppo34/src/gestioneGioco/Mazzo.java
+++ CodexGruppo34/src/gestioneGioco/Mazzo.java
@@ -1,4 +1,6 @@
 package gestioneGioco;
+import java.util.Random;
+
 import componentiGioco.Carta;
 
 public class Mazzo {
@@ -9,7 +11,20 @@ public class Mazzo {
     	this.carte=new Carta[numero];
     	this.carte=carte;
     	
+ 
     	
     }
+
+	public void mischiaMazzo() {
+		Random rn = new Random();
+		
+		for(int i=0; i<this.carte.length;i++) {
+			int nuovoIndice = rn.nextInt(this.carte.length);
+          Carta carta = this.carte[nuovoIndice];
+          this.carte[nuovoIndice] = this.carte[i];
+          this.carte[i] = carta;
+		}
+	
+	}
 
 }
